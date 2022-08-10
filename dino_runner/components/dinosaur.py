3801f867e2776ed165e054d354dc371d1a21c5f0
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2022-08-10 19:32:07 -0400

    Clase 2 10/08/22

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -7,6 +7,8 @@ class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     Y_POS_DUCK = 340
+    JUMP_VEL = 8.5
+
 
     def __init__(self):
         self.image = RUNNING [0]
@@ -16,12 +18,14 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_duck = False
+        self.dino_jump = False
+        self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
         if self.dino_duck:
             self.duck()
 
-        if self.dino_run():
+        if self.dino_run:
             self.run()
         
         if self.dino_jump:
@@ -72,5 +76,5 @@ class Dinosaur(Sprite):
 
 
     def draw(self, screen):
-        screen.blit(self, image, (self.dino_rect.x, self.dino_rect.y))
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 class Game:
     def __init__(self):
         pygame.init()
@@ -14,6 +15,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,14 +32,16 @@ class Game:
                 self.playing = False
 
     def update(self):
-        user_input = pygame.key.get_pressed
+        user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
